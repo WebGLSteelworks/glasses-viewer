b8341d28210cd4f8e9984f712bc40203d3ed30e3
juliodeandres <hello@juliodean.com>
2026-09-16 14:50:02 +0200

    update viewer

--- src/config/models.config.js
+++ src/config/models.config.js
@@ -163,22 +163,45 @@ export const MODELS = {
 	},
   },
   
-    HSTN: {
-    label:         'HSTN',
-    glb:           'models/Standard_HSTN.glb',
-    hdri:          'studio_starfire_4k.hdr',
+    SKYLER: {
+    label:         'Skyler',
+    glb:           'models/Standard_Skyler.glb',
+    hdri:          'studio_wayfarer_2k.hdr',
     hdriIntensity: 0.75,
     startCamera:   'Cam_Front',
     cameras:       ADVENTURER_CAMERAS,
     fresnel:       null,
     glass:         { animate: true },
-	variantOrder: ['Frame_Classic_Black', 'Frame_Dark_Havana', 'Lens_Black', 'Lens_Chocolate' , 'Lenses_Transitions_Grey'],
+	variantOrder: ['Frame_Shiny_Black', 'Frame_Shiny_Chalky_Gray', 'Frame_Shiny_Mystic_Violet', 'Frame_Shiny_Transparent_Peach', 
+	'Lenses_Clear', 'Lenses_Green', 'Lenses_Polar_Brown', 'Lenses_Polar_Dusty_Blue','Lenses_Polar_Dusty_Red', 'Lenses_Polar_Green', 
+	'Lenses_Charcoal_Black', 'Lenses_Polar_Gradient_Graphite', 'Lenses_Clear_Green', 'Lenses_Clear_Grey', 'Lenses_Clear_Sapphire',
+	'Lenses_Clear_Amethyst', 'Lenses_Clear_Emerald', 'Lenses_Brown_Transitions', 'Lenses_Gradient_Graphite', 'Lenses_Clear_Graphite_Green'],
 	shadow: {
 	  enabled:   true,
 	  intensity: 1.0,
 	  softness:  1.0,
 	},
   }
+  
+    // HSTN: {
+    // label:         'HSTN',
+    // glb:           'models/Standard_HSTN.glb',
+    // hdri:          'studio_starfire_4k.hdr',
+    // hdriIntensity: 0.75,
+    // startCamera:   'Cam_Front',
+    // cameras:       ADVENTURER_CAMERAS,
+    // fresnel:       null,
+    // glass:         { animate: true },
+	// variantOrder: ['Frame_Shiny_Black', 'Frame_Shiny_Chalky_Gray', 'Frame_Shiny_Mystic_Violet', 'Frame_Shiny_Transparent_Peach', 
+	// 'Lenses_Clear', 'Lenses_Green', 'Lenses_Polar_Brown', 'Lenses_Polar_Dusty_Blue','Lenses_Polar_Dusty_Red', 'Lenses_Polar_Green', 
+	// 'Lenses_Charcoal_Black', 'Lenses_Polar_Gradient_Graphite', 'Lenses_Clear_Green', 'Lenses_Clear_Grey', 'Lenses_Clear_Sapphire',
+	// 'Lenses_Clear_Amethyst', 'Lenses_Clear_Emerald', 'Lenses_Brown_Transitions', 'Lenses_Gradient_Graphite', 'Lenses_Clear_Graphite_Green'],
+	// shadow: {
+	  // enabled:   true,
+	  // intensity: 1.0,
+	  // softness:  1.0,
+	// },
+  // }
 
 };
 
