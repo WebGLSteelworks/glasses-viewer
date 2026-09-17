044ed3a96ee22af006ed5cac00e64214918b19b6
juliodeandres <hello@juliodean.com>
2026-09-17 15:30:25 +0200

    update viewer

--- src/config/models.config.js
+++ src/config/models.config.js
@@ -175,13 +175,35 @@ export const MODELS = {
 	variantOrder: ['Frame_Shiny_Black', 'Frame_Shiny_Chalky_Gray', 'Frame_Shiny_Mystic_Violet', 'Frame_Shiny_Transparent_Peach', 
 	'Lenses_Clear', 'Lenses_Green', 'Lenses_Polar_Brown', 'Lenses_Polar_Dusty_Blue','Lenses_Polar_Dusty_Red', 'Lenses_Polar_Green', 
 	'Lenses_Charcoal_Black', 'Lenses_Polar_Gradient_Graphite', 'Lenses_Clear_Green', 'Lenses_Clear_Grey', 'Lenses_Clear_Sapphire',
-	'Lenses_Clear_Amethyst', 'Lenses_Clear_Emerald', 'Lenses_Brown_Transitions', 'Lenses_Gradient_Graphite', 'Lenses_Clear_Graphite_Green'],
+	'Lenses_Clear_Amethyst', 'Lenses_Clear_Emerald', 'Lenses_Clear_Brown_Transitions', 'Lenses_Gradient_Graphite', 'Lenses_Clear_Graphite_Green'],
+	shadow: {
+	  enabled:   true,
+	  intensity: 1.0,
+	  softness:  1.0,
+	},
+  },
+
+    HEADLINER: {
+    label:         'Headliner',
+    glb:           'models/Standard_Headliner.glb',
+    hdri:          'studio_wayfarer_2k.hdr',
+    hdriIntensity: 0.75,
+    startCamera:   'Cam_Front',
+    cameras:       ADVENTURER_CAMERAS,
+    fresnel:       null,
+    glass:         { animate: true },
+	variantOrder: ['Frame_Shiny_Black', 'Frame_Matte_Black', 'Frame_Shiny_Asteroid_Grey', 'Frame_Matte_Transparent_Peach', 
+	'Lenses_Clear', 'Lenses_Green', 'Lenses_Polar_Brown', 'Lenses_Polar_Dusty_Blue','Lenses_Polar_Dusty_Red', 'Lenses_Polar_Green', 
+	'Lenses_Charcoal_Black', 'Lenses_Polar_Gradient_Graphite', 'Lenses_Clear_Green', 'Lenses_Clear_Grey', 'Lenses_Clear_Sapphire',
+	'Lenses_Clear_Amethyst', 'Lenses_Clear_Emerald', 'Lenses_Clear_Brown_Transitions', 'Lenses_Gradient_Graphite', 'Lenses_Clear_Graphite_Green'],
 	shadow: {
 	  enabled:   true,
 	  intensity: 1.0,
 	  softness:  1.0,
 	},
   }
+
+
   
     // HSTN: {
     // label:         'HSTN',
